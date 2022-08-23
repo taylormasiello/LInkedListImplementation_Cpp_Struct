829158f11d5834a5c97da18e8c6ddd11b44dd82c
Taylor Masiello <masiellotay@gmail.com>
2022-08-23 09:24:53 -0700

    malloc, more C version

--- LInkedListImplementation_Cpp_Struct/LInkedListImplementation_Cpp_Struct.cpp
+++ LInkedListImplementation_Cpp_Struct/LInkedListImplementation_Cpp_Struct.cpp
@@ -11,6 +11,7 @@ Node A = { 1, NULL }; //A points no where, list is empty; had to construct to re
 
 //allocates mem (memory block), given sizeof(Node); dynamically allocated mem, allocated at runtime; can only work w/ this type of memory w/ pointers & ref's
 //malloc() returns a void pointer that gives address of the assgined mem block; cast to "pointer to Node" from "void pointer"
+//C version
 Node* temp = (Node*)malloc(sizeof(Node)); 
 
 int main()
