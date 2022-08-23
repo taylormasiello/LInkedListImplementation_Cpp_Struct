c252c149e484692a91747559494119f725c7deeb
Taylor Masiello <masiellotay@gmail.com>
2022-08-23 09:29:34 -0700

    C++ implentation w/ struct

--- LInkedListImplementation_Cpp_Struct/LInkedListImplementation_Cpp_Struct.cpp
+++ LInkedListImplementation_Cpp_Struct/LInkedListImplementation_Cpp_Struct.cpp
@@ -6,16 +6,17 @@ struct Node
     Node* link;
 };
 
-Node* A; //pointer to Node A
-Node A = { 1, NULL }; //A points no where, list is empty; had to construct to remove red underline
 
-//allocates mem (memory block), given sizeof(Node); dynamically allocated mem, allocated at runtime; can only work w/ this type of memory w/ pointers & ref's
-//malloc() returns a void pointer that gives address of the assgined mem block; cast to "pointer to Node" from "void pointer"
-//C version
-Node* temp = (Node*)malloc(sizeof(Node)); 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Node* A; //pointer to Node A
+    A = NULL; //A points no where, list is empty
+
+    Node* temp = new Node(); //new syntax of C++ preferred to grab mem blocks
+    temp->data = 2;
+    temp->link = NULL;
+
+    A = temp;
 }
 
